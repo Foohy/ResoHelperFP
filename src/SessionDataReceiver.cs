4c7c3f49e8e34dd9832bd414fa03b98c09c82188
Nutcake <Nusskuchen95@hotmail.de>
2024-01-13 21:56:57 +0100

    Improve http status handler

--- src/SessionDataReceiver.cs
+++ src/SessionDataReceiver.cs
@@ -26,13 +26,23 @@ public class SessionDataReceiver
     {
         var context = await _listener.GetContextAsync();
         var hostname = context.Request.Url?.Segments.Last();
+        if (hostname == "ingest/")
+        {
+            await Respond(context.Response, "Failure"u8.ToArray());
+            return;
+        }
+
         var body = await new StreamReader(context.Request.InputStream).ReadToEndAsync();
         UniLog.Log($"Received session data: {body}");
         var data = JsonConvert.DeserializeObject<Dictionary<string, SessionData>>(body) ??
                    new Dictionary<string, SessionData>();
         SessionsUpdated?.Invoke(hostname ?? "Unkown", data);
         var response = context.Response;
-        var buffer = "Success"u8.ToArray();
+        await Respond(context.Response, "Success"u8.ToArray());
+    }
+
+    private async Task Respond(HttpListenerResponse response, byte[] buffer)
+    {
         response.ContentLength64 = buffer.Length;
         var output = response.OutputStream;
         await output.WriteAsync(buffer);
